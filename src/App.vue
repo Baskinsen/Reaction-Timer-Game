cc0ab92b927cd13683786582920d5c44b5435f70
babalolaay0 <92514412+babalolaay0@users.noreply.github.com>
2022-08-16 11:31:33 -0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1>Reaction Timer Game</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay ="delay" @end="endGame"/>
+  <Result v-if="showResult" :score="score"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
+
+  export default {
+    name: 'app',
+    components:{ Block, Result} ,
+    data() {
+      return {
+       isPlaying: false,
+       delay: null,
+       score: null,
+       showResult: false
+      }
+    },
+   methods: {
+        start() {
+          this.isPlaying = true
+          this.delay = 2000 + Math.random() * 5000
+          this.showResult = false
+        }, 
+        endGame(reactionTime) {
+          this.score = reactionTime
+          this.isPlaying = false
+          this.showResult = true
+        }
+     }
+   }
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
+  
---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+<p>Reaction TIme: {{score}} ms</p>
+<p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 200) {
+            this.rank = 'Ninja Rapid Fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Fingers'
+        } else {
+            this.rank = 'Snail Fingers'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
